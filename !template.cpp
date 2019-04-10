84c4a7ee426f18570c6efba41671bb1dbe2ab3a0
cometeme <cometemeJackson@outlook.com>
2019-04-10 21:36:07 +0800

    Create !template.cpp

---
+++ !template.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+#define INF 0x3f3f3f3f
+#define EXP 1e-8
+
+#define ll long long
+
+#define loop(start, end, step) for(int i = (start); i < (end); i += (step))
+#define aloop(content) for(auto cur : content)
+#define update_max(cur, max) if((cur) > max) max = (cur);
+#define update_min(cur, min) if((cur) < min) min = (cur);
+#define max(x, y)  (((x) > (y)) ? (x) : (y))
+#define min(x, y)  (((x) < (y)) ? (x) : (y))
+
+int main()
+{
+    ios :: sync_with_stdio(false);
+
+
+    return 0;
+}
