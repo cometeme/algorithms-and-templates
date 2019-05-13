29f22ac17fcff381708a2170e0df7cc150519781
cometeme <cometemeJackson@outlook.com>
2019-05-13 15:41:33 +0800

    template updated

--- !template.cpp
+++ !template.cpp
@@ -10,19 +10,17 @@ using namespace std;
 
 #define ll long long
 
-#define memclr(_var)  memset(_var, 0, sizeof(_var))
-#define maximize(_var, _cur)  _var = max(_var, _cur)
-#define minimize(_var, _cur)  _var = min(_var, _cur)
-#define reps(_var, _begin, _end,_step)  for(int _var = (_begin); _var <= (_end); _var += (_step))
-#define reps_(_var, _end, _begin,_step)  for(int _var = (_end); _var >= (_begin); _var -= (_step))
-#define rep(_var, _begin, _end)  reps(_var, _begin, _end, 1)
-#define rep_(_var, _end, _begin)  reps_(_var, _end, _begin, 1)
+#define memclr(_var) memset(_var, 0, sizeof(_var))
+#define maximize(_var, _cur) _var = max(_var, _cur)
+#define minimize(_var, _cur) _var = min(_var, _cur)
+#define reps(_var, _begin, _end, _step) for (int _var = (_begin); _var <= (_end); _var += (_step))
+#define reps_(_var, _end, _begin, _step) for (int _var = (_end); _var >= (_begin); _var -= (_step))
+#define rep(_var, _begin, _end) reps(_var, _begin, _end, 1)
+#define rep_(_var, _end, _begin) reps_(_var, _end, _begin, 1)
 
 int main(int argc, char *argv[])
 {
     ios::sync_with_stdio(false);
-
-
 
     return 0;
 }
