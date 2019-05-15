f077aa8d85721f61e20c708f85986c61ce5cd499
cometeme <cometemeJackson@outlook.com>
2019-05-15 15:10:54 +0800

    remove files

--- !make_test_file.py
+++
@@ -1,9 +0,0 @@
-template_path = '!template.cpp'
-pre = 'test'
-max_i = 3
-
-with open(template_path, 'r') as file:
-    code = file.read()
-    for i in range(max_i):
-        with open('{}_{}.cpp'.format(pre, chr(65 + i)), mode='w') as file:
-            file.write(code)
--- !remove_all_build_file.py
+++
@@ -1,7 +0,0 @@
-from os import listdir, remove
-
-files = listdir()
-
-for file in files:
-    if('.' not in file or file.endswith("exe")):
-        remove(file)
