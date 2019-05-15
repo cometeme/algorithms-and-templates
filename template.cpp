98a22a5132dd26963f269a3ff7721357745ccd8e
cometeme <cometemeJackson@outlook.com>
2019-05-15 15:10:46 +0800

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,41 @@
+#include <iostream> // C++ I/O
+#include <fstream>  // File I/O
+#include <sstream>  // String stream I/O
+#include <iomanip>  // C++ I/O manipulator
+
+#include <cstdlib> // C library
+#include <cstdio>  // C I/O
+#include <ctime>   // C time
+#include <cmath>   // Math library
+#include <cstring> // C strings
+
+#include <vector>    // Vector
+#include <queue>     // Queue
+#include <stack>     // Stack
+#include <map>       // Map
+#include <set>       // Set
+#include <algorithm> // Algorithms
+
+using namespace std;
+
+#define INF 0x3f3f3f3f
+#define EXP 1e-8
+
+#define ll long long
+
+#define memclr(_var) memset(_var, 0, sizeof(_var))
+#define maximize(_var, _cur) _var = max(_var, _cur)
+#define minimize(_var, _cur) _var = min(_var, _cur)
+#define reps(_var, _begin, _end, _step) for (int _var = (_begin); _var <= (_end); _var += (_step))
+#define reps_(_var, _end, _begin, _step) for (int _var = (_end); _var >= (_begin); _var -= (_step))
+#define rep(_var, _begin, _end) reps(_var, _begin, _end, 1)
+#define rep_(_var, _end, _begin) reps_(_var, _end, _begin, 1)
+
+int main(int argc, char *argv[])
+{
+    ios::sync_with_stdio(false);
+
+    
+
+    return 0;
+}
