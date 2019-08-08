64c294ce75359d1084eda9bd9b3cd34dc7508d25
cometeme <cometemeJackson@outlook.com>
2019-08-08 19:04:46 +0800

    update template

--- template.cpp
+++ template.cpp
@@ -1,14 +1,14 @@
-#include <iostream>  // C++ I/O
-#include <string>    // C++ string
-#include <fstream>   // File I/O
-#include <sstream>   // String stream I/O
-#include <iomanip>   // C++ I/O manipulator
+#include <iostream> // C++ I/O
+#include <string>   // C++ string
+#include <fstream>  // File I/O
+#include <sstream>  // String stream I/O
+#include <iomanip>  // C++ I/O manipulator
 
-#include <cstdlib>   // C library
-#include <cstdio>    // C I/O
-#include <ctime>     // C time
-#include <cmath>     // Math library
-#include <cstring>   // C strings
+#include <cstdlib> // C library
+#include <cstdio>  // C I/O
+#include <ctime>   // C time
+#include <cmath>   // Math library
+#include <cstring> // C strings
 
 #include <vector>    // Vector
 #include <queue>     // Queue
@@ -21,9 +21,10 @@
 using namespace std;
 
 #define INF 0x3f3f3f3f
-#define EXP 1e-8
+#define EPS 1e-8
 
-#define ll long long
+typedef long long ll;
+typedef unsigned long long ull;
 
 #define memclr(_var) memset(_var, 0, sizeof(_var))
 #define maximize(_var, _cur) _var = max(_var, _cur)
@@ -33,11 +34,27 @@ using namespace std;
 #define rep(_var, _begin, _end) reps(_var, _begin, _end, 1)
 #define rep_(_var, _end, _begin) reps_(_var, _end, _begin, 1)
 
+int read()
+{
+    char ch = getchar();
+    int x = 0, f = 1;
+    while (ch < '0' || ch > '9')
+    {
+        if (ch == '-')
+            f = -1;
+        ch = getchar();
+    }
+    while (ch >= '0' && ch <= '9')
+        x = x * 10 + ch - '0', ch = getchar();
+    return x * f;
+}
+
 int main(int argc, char *argv[])
 {
     ios::sync_with_stdio(false);
+    cin.tie(0);
 
-    
+
 
     return 0;
 }
